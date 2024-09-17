068cf903c90d1fb7f6de7a44e2b609842bcf0732
cemdurakDEV <cemdurakwork@gmail.com>
2024-09-17 12:51:23 +0300

    17 Eylül

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+//CORE KATMANI DİĞER KATMANLARI REFERANS ALMAZ
+namespace Core.DataAccess
+{
+    //Generic Repository
+
+    //Generic Constraint
+      //class:referans tip olabilir anlamında
+      //IEntity: IEnttity olabilir yada IEntity implemente eden bir nesne olabilir
+      //new() : new'lenebilir olmalı. Interface new'lenemez. IEntıty kullanılamaz sadece implemente eden nesneler kullanabilir.
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null); // null anlamı filtre vermeyedebiliriz anlamında
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    } 
+}
